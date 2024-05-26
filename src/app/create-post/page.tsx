c7033c49a636b2b5249e967f8dcec92ecdc9ee7c
Jeff Lee <lgf2111@gmail.com>
2024-05-26 18:37:29 +0800

    first commit

---
+++ src/app/create-post/page.tsx
@@ -0,0 +1,12 @@
+import Form from "@/components/form";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
+export default async function Page() {
+  return (
+    <main className="text-center pt-16">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">Create Post</h1>
+      <Form />
+      <LogoutLink>Log out</LogoutLink>
+    </main>
+  );
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,14 @@
+import Form from "@/components/form";
+import PostsList from "@/components/posts-list";
+import { Suspense } from "react";
+
+export default async function Page() {
+  return (
+    <main className="text-center pt-16 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
+      <Suspense fallback="Loading...">
+        <PostsList />
+      </Suspense>
+    </main>
+  );
+}
